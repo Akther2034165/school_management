b9a197854a8b07425d90f62c7704d7491ecef0e6
unknown <Akther2034165>
2023-07-01 00:26:53 +0600

    first commit

---
+++ concept.py
@@ -0,0 +1,21 @@
+"""
+School system development
+- - - - - - - - - -- - - -
+    High Level Areas
+   - - - - - - - - - -
+1. student management
+2. teacher management
+3. cafeteria
+4. sports management
+5. club management
+6. event management
+7. library management
+8. assessment 
+- - - - - - - - - - - - -
+core functionalities
+1. There will be a classroom consisting of multiple students
+2. Student can enroll multiple classes
+3. Teacher will take classes, they will take exams, they grade the exams
+4. System will generate the overall grade
+- - - - - - - - - - - - -
+"""
---
+++ relationship.py
@@ -0,0 +1,13 @@
+"""
+1. Association
+car --> driver
+classroom --> teacher
+
+
+2. Aggreation
+classroom is a collection of students.(has a relation)
+
+
+3. Composition
+as a part. Human, Heart. must thaktei hobe
+"""
